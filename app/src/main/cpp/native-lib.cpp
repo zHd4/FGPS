a512908a3fd87605f5cd6a2024c798abd2a39f26
zHd4 <computersystembase@gmail.com>
2020-08-18 18:47:19 +0300

    activity_main.xml renamed to main_activity.xml Fixed native-lib.cpp package error

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,13 +3,13 @@
 #include <string>
 
 extern "C" JNIEXPORT jdouble JNICALL
-Java_com_github_zhd4_fgps_models_geo_Geo_randomLatitude(JNIEnv* env, jobject) {
+Java_com_fgps_models_geo_Geo_randomLatitude(JNIEnv* env, jobject) {
     Geo geo;
     return geo.generateRandomLatitude();
 }
 
 extern "C" JNIEXPORT jdouble JNICALL
-Java_com_github_zhd4_fgps_models_geo_Geo_randomLongitude(JNIEnv* env, jobject) {
+Java_com_fgps_models_geo_Geo_randomLongitude(JNIEnv* env, jobject) {
     Geo geo;
     return geo.generateRandomLongitude();
 }
